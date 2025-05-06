7ba4d09405701bdf46bb623587c27f4603ab43e4
DANIEL FILIPE ALVES <daniel.falves3@senacsp.edu.br>
2025-05-06 20:09:14 -0300

    mudando a versção do dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from "react- router-dom"
 import {Footer} from "./components/Footer";
 import {Header} from "./components/Header";
 import {Contato, Time} from "./pages/Time";
+import {Contato, Time} from "./pages/Contato";
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
           <Routes />
           {/* pagina inicial só com header e footer */}
              <Route path="/" element={<></>} />
-             <Route path="/contato" element={<Contato />} />
+             <Route path="/Contato" element={<Contato />} />
              <Route path="/Time" element={<Time />} />
           <Routes/>
       <Footer />
--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -2,8 +2,30 @@ import styles from "./Contato.module.css";
 
 export function Contato() {
   return (
-   <>
-   
+  <>
+    <section id="contact" class="text-image-section">
+      <div id="contact-image-container" class="image-container">
+        <img src="images/office.jpg" alt="Advogados no escritório" />
+      </div>
+      <div id="contact-information" class="text-container">
+        <i className={bi bi-envelope}></i>
+        <p className={section-subtitle}>Entre em contato</p>
+        <form>
+          <div className={form-control}>
+            <input type="text" name="name" id="name" placeholder="Nome" />
+            <input type="email" name="email" id="email" placeholder="E-mail" />
+          </div>
+          <input type="text" name="phone" id="phone" placeholder="Telefone" />
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Sua mensagem..."
+          ></textarea>
+          <input type="submit" value="Enviar" class="btn" />
+        </form>
+      </div>
+    </section>
+
    </>
   )
 }
