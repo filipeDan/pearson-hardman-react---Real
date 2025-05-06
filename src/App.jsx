45f8f54b4bc03a2232b6dcb8baa3c94b2651cb91
DANIEL FILIPE ALVES <daniel.falves3@senacsp.edu.br>
2025-05-06 20:37:31 -0300

    arrumando o APP.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // na instalação do " react- router-dom" 
-import { BrowserRouter as Router, Routes, Route} from "react- router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {Footer} from "./components/Footer";
 import {Header} from "./components/Header";
 import {Time} from "./pages/Time";
